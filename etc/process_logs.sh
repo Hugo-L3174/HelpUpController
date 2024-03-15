f2a67b06e9bcc70127c40d8d14a340d9ce76acac
Hugo L <lefevrehugo@ymail.com>
2024-03-15 15:44:52 +0100

    script to reprocess logs with only shoes and xsens data

---
+++ etc/process_logs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# check arguments
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <input_log1> [<input_log2> ...]"
+    exit 1
+fi
+
+if ! command -v mc_bin_utils &> /dev/null; then
+    echo "Error: mc_bin_utils is not installed."
+    exit 1
+fi
+
+# keys to keep in reprocessed log
+keys=("XsensPlugin_raw_*" "ForceShoes_*")
+
+for input_log in "$@"; do
+    echo "Processing ${input_log}"
+    name_bin=$input_log
+    name_only="${name_bin%.*}" #remove extension for output name
+    mc_bin_utils extract "$input_log" "${name_only}_processed.bin" --keys "${keys[@]}"
+    echo "Created ${name_only}_processed.bin"
+done
+
+echo "Processed all logs."
